a9ad3857e78517d8801802b251c97a1a18374426
Alexander Varela <jkaalexkei@gmail.com>
2021-03-30 07:23:54 -0500

    Iniciamos trabajos con python

---
+++ strings.py
@@ -0,0 +1,14 @@
+mystr = "Hola"
+
+#print(dir(mystr))
+
+print(mystr.upper())
+
+#la palabra resevada dir nos permite identificar las propiedades y metodos que se pueden usar con un tipo de dato determinado
+# valor = 20
+# print(dir(valor))
+
+print(mystr.replace("Hola","alex").upper())#metodos encadenados
+
+
+
---
+++ variables.py
@@ -0,0 +1,21 @@
+# nombre = "Alex"
+
+# print(nombre)
+
+# #MANERAS DE DECLARAR VARIABLES
+
+# x = 10
+# texto = "alex"
+# x,texto = 10, "hola"
+
+# #Manera de llamar Variables
+# palabras unidas por un guion bajo se les denomina snake case ejemplo nombre_libro
+
+# primera palabra en minusculas e inicial de la segunda en Mayusculas se le llama camel case ejemplo-> nombreLibro
+
+# Inicial de la primera palabra en Mayusculas e inicial de la segunda Palabra en Mayusculas se le denomina Pascal Case ejemplo-> NombreLibro
+
+PARA EL NOMBRE DE LAS CONSTANTES SE PUEDE USAR CON TODAS LAS LETRAS EN MAYUSCULAS EJEMPLOS-> PI=3.1416
+
+
+ES DINAMICO PORQUE PERMITE QUE EL VALOR DE LAS VARIABLES SE PUEDA REASIGNAR

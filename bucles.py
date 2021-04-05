9d82ea722080847d154b0ef51aebd9907d1ce64c
Alexander Varela <jkaalexkei@gmail.com>
2021-04-04 23:04:15 -0500

    se agrego bucles y conexion a base de datos en mysql

---
+++ bucles.py
@@ -0,0 +1,19 @@
+lista = [1,2,3,4]
+print(type(lista))
+
+for milista in lista:
+    print(milista)
+
+valores = {'A': 4, 'E': 3, 'I': 1, 'O': 0}
+for k, v in valores.items():
+    print('k=', k, ', v=', v)
+
+datos = {'nombre':'alex', 'apellido':'varela'}
+
+for nom, ape in datos.items():
+    print(nom, ' ', ape)
+
+
+
+
+
---
+++ diccionarios.py
@@ -0,0 +1,31 @@
+dic = {
+
+    "nombre":"Alex"
+}
+print(dic)
+
+dic.update({"nombre":"gabriel"})
+
+print(dir(dic))
+
+#num = input()
+
+# if int(num) < 10 or int(num) > 20:
+#     print("el numero esta fuera del rango")
+# else:
+#     print(f"el numero {num} esta entre 10 y 20")
+
+lista = (1,2,3,4,5,6,7)
+dicc = {"nombre": "alex","apellido": "varela","nombre":"keidy","apellido":"castro"}
+
+
+
+# metodos y propiedades diccionarios
+
+# ['__class__', '__contains__', '__delattr__', '__delitem__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__getitem__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__iter__', '__le__', '__len__', '__lt__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__reversed__', '__setattr__', '__setitem__', '__sizeof__', '__str__', '__subclasshook__', 'clear', 'copy', 'fromkeys', 'get', 'items', 'keys', 'pop', 'popitem', 'setdefault', 'update', 'values']
+
+
+
+
+
+
